74019e9946f2380f66d9b2cc86a71c707d7ddfa4
LeafInSummer <leafinsummer.github@gmail.com>
2017-01-11 00:15:41 -0800

    Add smaple images

---
+++ copy_file.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+for ((i=1;i<=11;i++));
+do
+#   cp ~/Desktop/sample_results/chuanghua/sample${i}.jpg sample${i}/1.jpg
+#   cp ~/Desktop/sample_results/deco/sample${i}.jpg sample${i}/2.jpg
+#   cp ~/Desktop/sample_results/fireworks/sample${i}.jpg sample${i}/3.jpg
+#   cp ~/Desktop/sample_results/oil/sample${i}.jpg sample${i}/4.jpg
+   cp ~/Desktop/sample_results/fountain/sample${i}.jpg sample${i}/5.jpg
+   cp ~/Desktop/sample_results/laketahoe/sample${i}.jpg sample${i}/6.jpg
+   cp ~/Desktop/sample_results/liuli/sample${i}.jpg sample${i}/7.jpg
+   cp ~/Desktop/sample_results/nihong/sample${i}.jpg sample${i}/8.jpg
+   cp ~/Desktop/sample_results/pattern/sample${i}.jpg sample${i}/9.jpg
+   cp ~/Desktop/sample_results/qinghuaci/sample${i}.jpg sample${i}/10.jpg
+   cp ~/Desktop/sample_results/shuimo/sample${i}.jpg sample${i}/11.jpg
+   cp ~/Desktop/sample_results/shuimo1/sample${i}.jpg sample${i}/12.jpg
+   cp ~/Desktop/sample_results/shuimo2/sample${i}.jpg sample${i}/13.jpg
+   cp ~/Desktop/sample_results/shuimo3/sample${i}.jpg sample${i}/14.jpg
+   cp ~/Desktop/sample_results/shuimo5/sample${i}.jpg sample${i}/15.jpg
+   cp ~/Desktop/sample_results/steel/sample${i}.jpg sample${i}/16.jpg
+   cp ~/Desktop/sample_results/wave/sample${i}.jpg sample${i}/17.jpg
+   cp ~/Desktop/sample_results/yindu/sample${i}.jpg sample${i}/18.jpg
+   cp ~/Desktop/sample_results/youhua/sample${i}.jpg sample${i}/19.jpg
+done
